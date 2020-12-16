ade4ffe62b62156cdfa2182fc3edc08de6c8d706
Alex Rios <alexrios.dev@gmail.com>
2020-12-16 15:47:37 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,81 @@ import React, { Component, Fragment } from 'react';
 import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
+
 class App extends Component {
-  render() {
+  state = {
+    pizzas: [],
+    pizza: ''
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/pizzas')
+    .then(resp => resp.json())
+    .then(pizzas => this.setState({ pizzas: pizzas }))
+  }
+
+  editPizza = (id) => {
+    let pizza = this.state.pizzas.find(pizza => pizza.id === id)
+    this.setState({ pizza: pizza })
+    
+    // OR
+    // fetch(`http://localhost:3000/pizzas/${pizza_id}`)
+    // .then(resp => resp.json())
+    // .then(pizza => this.setState({ pizza: pizza }))
+  }
+
+  handleChange = (e) => {    
+    this.setState({
+      pizza: {
+        ...this.state.pizza,
+        [e.target.name]: e.target.value,
+      }
+    })
+  }
+
+  valueSwitch = (value)  => {
+    switch (value) {
+      case 'Vegetarian':
+        return true;
+      case 'Not Vegetarian':
+        return false;
+    }
+  }
+
+  handleBoolean = (e) => {
+    this.setState({
+      pizza: {
+        ...this.state.pizza,
+        vegetarian: this.valueSwitch(e.target.value)
+      }
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    let pizzas = [...this.state.pizzas]
+    let index = pizzas.findIndex(pizza => pizza.id === this.state.pizza.id)
+    pizzas[index] = this.state.pizza
+    this.setState({
+      pizzas: pizzas
+    })
+    let options = {
+      method: 'PATCH',      
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(this.state.pizza),
+    }
+    fetch(`http://localhost:3000/pizzas/${this.state.pizza.id}`, options)
+  }
+
+  render() {    
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm pizza={this.state.pizza} handleChange={this.handleChange} handleBoolean={this.handleBoolean} handleSubmit={this.handleSubmit} />
+        <PizzaList pizzas={this.state.pizzas} editPizza={this.editPizza} />
       </Fragment>
     );
   }
--- src/containers/PizzaList.js
+++ src/containers/PizzaList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Pizza from '../components/Pizza'
-class PizzaList extends Component {
 
+class PizzaList extends Component {
   render() {
     return (
       <table className="table table-striped">
@@ -14,9 +14,7 @@ class PizzaList extends Component {
           </tr>
         </thead>
         <tbody>
-          {
-            //render Pizza here
-          }
+          {this.props.pizzas.map(pizza => <Pizza key={pizza.id} pizza={pizza} editPizza={this.props.editPizza}/>)}
         </tbody>
       </table>
     );
